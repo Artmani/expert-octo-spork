45b0ef55b2cd9f875a47d40f6f126444a0d165d2
Artem Lushkiv <Artmani1337@mail.ru>
2024-09-05 16:10:26 +0300

    swiperjs 320 done

---
+++ swiperInit.js
@@ -0,0 +1,44 @@
+if (window.innerWidth < 768) {
+    document.addEventListener("DOMContentLoaded", function () {
+        const swiper = new Swiper(".mySwiper", {
+            slidesPerView: "auto",
+            spaceBetween: 16,
+            slidesOffsetBefore: 16,
+            slidesOffsetAfter: 16,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    });
+}
+if (window.innerWidth > 767) {
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const toggleButton = document.querySelector('.services__link-button');
+        const toggleButtonText = toggleButton.querySelector('.services__link-text'); // Выбираем только span с классом
+        const slides = document.querySelectorAll('.swiper-slide');
+        let isExpanded = false;
+
+        // Скрываем лишние слайды по умолчанию (например, оставляем первые 4)
+        const toggleSlides = (showAll) => {
+            slides.forEach((slide, index) => {
+                if (index >= 4) {
+                    slide.style.display = showAll ? 'flex' : 'none'; // Показать все или скрыть лишние
+                }
+            });
+        };
+
+        // Изначально скрываем лишние элементы
+        toggleSlides(isExpanded);
+
+        // Обработчик клика на кнопку
+        toggleButton.addEventListener('click', function () {
+            isExpanded = !isExpanded;
+            toggleSlides(isExpanded);
+            toggleButtonText.innerText = isExpanded ? 'Скрыть' : 'Показать все'; // Меняем только текст внутри span с классом
+            toggleButton.classList.toggle('expanded', isExpanded); // Меняем класс для поворота стрелки
+        });
+    });
+    
+}
